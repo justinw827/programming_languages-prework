47f2cfe3a7ada8857d01f66acef33a13eb3b942a
Justin Wang <justinw827@aol.com>
2018-07-26 01:22:51 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,8 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = Hash.new
   languages.each do |key, val|
-    binding.pry
     styleName = key
+    binding.pry
     val.each do |key2, val2|
       binding.pry
       new_hash[key2] = val2
